f656c7e2c3e6b281914f673f16747c2d4051b35e
Geronimo Romney <g.romney@gmail.com>
2015-05-13 15:15:29 -0400

    GET User

--- lib/Ares.rb
+++ lib/Ares.rb
@@ -1,5 +1,12 @@
-require "Ares/version"
+#require "/Ares/version"
+require "open-uri"
+require "json"
 
 module Ares
   # Your code goes here...
+  @@url = 'http://www.codewars.com/api/v1'
+
+  def self.get_user (user)
+    respons = JSON.parse(open("#{@@url}/users/#{user}").read)
+  end
 end
