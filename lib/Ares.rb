68fffdb1b6b01d06126e321d1399bc89b8cd0012
Geronimo Romney <g.romney@gmail.com>
2015-05-13 15:20:35 -0400

    GET Code Challenge

--- lib/Ares.rb
+++ lib/Ares.rb
@@ -9,4 +9,8 @@ module Ares
   def self.get_user (user)
     respons = JSON.parse(open("#{@@url}/users/#{user}").read)
   end
+
+  def self.get_challenge (id)
+    respons = JSON.parse(open("#{@@url}/code-challenges/#{id}").read)
+  end
 end
