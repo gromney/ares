96a86eb0fee68b980b4d8104082143f7368a2801
Geronimo Romney <g.romney@gmail.com>
2015-05-14 13:44:22 -0400

    usando httparty para hacer los request

--- lib/Ares.rb
+++ lib/Ares.rb
@@ -1,16 +1,21 @@
 #require "/Ares/version"
-require "open-uri"
-require "json"
+#require "open-uri"
+#require "json"
+require "httparty"
 
 module Ares
   # Your code goes here...
-  @@url = 'http://www.codewars.com/api/v1'
+  include HTTParty
+  #@@url = 'http://www.codewars.com/api/v1'
+  base_uri 'http://www.codewars.com/api/v1'
 
   def self.get_user (user)
-    respons = JSON.parse(open("#{@@url}/users/#{user}").read)
+    #respons = JSON.parse(open("#{@@url}/users/#{user}").read)
+    get("/users/#{user}")
   end
 
   def self.get_challenge (id)
-    respons = JSON.parse(open("#{@@url}/code-challenges/#{id}").read)
+    #respons = JSON.parse(open("#{@@url}/code-challenges/#{id}").read)
+    get("/code-challenges/#{id}")
   end
 end
