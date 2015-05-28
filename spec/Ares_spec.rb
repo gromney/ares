230157d7e256811c6657b8ee97460037ad7932b7
Geronimo Romney <g.romney@gmail.com>
2015-05-28 14:20:09 -0400

    Add failing spec to retrieve description of a kata

--- spec/Ares_spec.rb
+++ spec/Ares_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Ares do
+  context 'when valid language' do
+    let(:language) {:ruby}
 
+    it "should return the description of todays kata" do
+      expect(Ares.train(language)[:description]).to eq ''
+    end
+  end
 end
